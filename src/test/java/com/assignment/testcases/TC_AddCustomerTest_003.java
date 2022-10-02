4c7cb1623dd7e963b7d279c4a58b0862f4b28a8a
gitsamim <41549129+gitsamim@users.noreply.github.com>
2022-10-02 20:46:36 +0530

    Update TC_AddCustomerTest_003.java

--- src/test/java/com/assignment/testcases/TC_AddCustomerTest_003.java
+++ src/test/java/com/assignment/testcases/TC_AddCustomerTest_003.java
@@ -34,47 +34,6 @@ public class TC_AddCustomerTest_003 extends BaseClass
 		
 		String s = exp1+exp2;
 		lp.validate(s);
-	/*	
-		addcust.clickAddNewCustomer();
-		
-		logger.info("providing customer details....");
-		
-		
-		addcust.custName("Samim");
-		addcust.custgender("male");
-		addcust.custdob("06","20","1980");
-		Thread.sleep(5000);
-		addcust.custaddress("INDIA");
-		addcust.custcity("KOLKATA");
-		addcust.custstate("WB");
-		addcust.custpinno("713131");
-		addcust.custtelephoneno("8918697677");
-		
-		String email=randomestring()+"@gmail.com";
-		addcust.custemailid(email);
-		addcust.custpassword("abcdef");
-		addcust.custsubmit();
-		
-		Thread.sleep(3000);
-		
-		logger.info("validation started....");
-		
-		boolean res=driver.getPageSource().contains("Customer Registered Successfully!!!");
-		
-		if(res==true)
-		{
-			Assert.assertTrue(true);
-			logger.info("test case passed....");
-			
-		}
-		else
-		{
-			logger.info("test case failed....");
-			captureScrenshot(driver,"addNewCustomer");
-			Assert.assertTrue(false);
-		}
-		
-		*/
 			
 	}
 	
